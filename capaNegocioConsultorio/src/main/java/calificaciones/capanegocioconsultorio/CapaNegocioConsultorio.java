666be86accc6c187b2822c5a6687f441886aea88
jj-om <jesus.osuna240549@potros.itson.edu.mx>
2025-02-19 20:05:13 -0700

    Creación de Capas: -capaPersitenciaConsultorio -capaNegocioConsultorio -capaPresentacionConsultorio

---
+++ capaNegocioConsultorio/src/main/java/calificaciones/capanegocioconsultorio/CapaNegocioConsultorio.java
@@ -0,0 +1,14 @@
+package calificaciones.capanegocioconsultorio;
+
+/*
+ * @author Carmen Beltran 
+ * @author Manuel Guerrero
+ * @author Jesús Osuna
+ */
+
+public class CapaNegocioConsultorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ capaPersistenciaConsultorio/src/main/java/calificaciones/capapersistenciaconsultorio/CapaPersistenciaConsultorio.java
@@ -0,0 +1,14 @@
+package calificaciones.capapersistenciaconsultorio;
+
+/*
+ * @author Carmen Beltran 
+ * @author Manuel Guerrero
+ * @author Jesús Osuna
+ */
+
+public class CapaPersistenciaConsultorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
