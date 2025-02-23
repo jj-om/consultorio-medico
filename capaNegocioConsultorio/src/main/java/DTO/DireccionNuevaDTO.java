7a7f16291cfb69f1b24532882ca801e1522e51e8
Daniel Buelna <>
2025-02-22 18:08:24 -0700

    Se completó la clase "PacienteNuevoDTO" y se añadió la clase "DireccionNuevaDTO".

---
+++ capaNegocioConsultorio/src/main/java/DTO/DireccionNuevaDTO.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+/**
+ *
+ * @author Daniel Buelna
+ */
+public class DireccionNuevaDTO {
+    
+    private String calle;
+    private int numero;
+    private String colonia;
+    
+    public DireccionNuevaDTO() {
+        
+    }
+
+    public DireccionNuevaDTO(String calle, int numero, String colonia) {
+        this.calle = calle;
+        this.numero = numero;
+        this.colonia = colonia;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    @Override
+    public String toString() {
+        return "DireccionNuevaDTO{" + "calle=" + calle + ", numero=" + numero + ", colonia=" + colonia + '}';
+    }
+}
--- capaNegocioConsultorio/src/main/java/DTO/PacienteNuevoDTO.java
+++ capaNegocioConsultorio/src/main/java/DTO/PacienteNuevoDTO.java
@@ -7,5 +7,67 @@ package DTO;
  */
 
 public class PacienteNuevoDTO {
+    
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String correoElectronico;
+    private String telefono;
+    
+    public PacienteNuevoDTO() {
+        
+    }
 
+    public PacienteNuevoDTO(String nombres, String apellidoPaterno, String apellidoMaterno, String correoElectronico, String telefono) {
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.correoElectronico = correoElectronico;
+        this.telefono = telefono;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "PacienteNuevoDTO{" + "nombres=" + nombres + ", apellidoPaterno=" + apellidoPaterno + ", apellidoMaterno=" + apellidoMaterno + ", correoElectronico=" + correoElectronico + ", telefono=" + telefono + '}';
+    }
 }
